3a0d9f77135589a2a40f11298fb7f6f103755941
Ajay Karode <Ajay.K_INT1595@accolite.com>
2024-04-10 15:37:14 +0530

    'initial

---
+++ ConsoleAppAssign/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace ConsoleAppAssign
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number : ");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number : ");
+            int number2 = int.Parse(Console.ReadLine());
+            int result = AddNumbers(number1, number2);
+
+            Console.WriteLine("The sum of " + number1 + " and " + number2 + " is " + result);
+
+        }
+        public static int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
+
